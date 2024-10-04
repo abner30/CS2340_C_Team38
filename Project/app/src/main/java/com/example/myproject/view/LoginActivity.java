0c968d9d97d8f333c05a86d16207cf5e8a3aabba
Li-Chen-Chang <lchang78@gatech.edu>
2024-10-04 16:40:20 -0400

    Added Javadoc for RegistrationActivity.java

--- Project/app/src/main/java/com/example/myproject/view/LoginActivity.java
+++ Project/app/src/main/java/com/example/myproject/view/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.myproject.view;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -39,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
