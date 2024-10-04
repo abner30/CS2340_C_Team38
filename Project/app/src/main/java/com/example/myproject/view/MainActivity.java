ec0271609c42a828eb97915147c6378948201da2
kb8767 <krishnahbhanderi@gmail.com>
2024-10-04 15:56:11 -0400

    Update MainActivity
    Include button function to open login page

--- Project/app/src/main/java/com/example/myproject/view/MainActivity.java
+++ Project/app/src/main/java/com/example/myproject/view/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.myproject.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-
-import com.example.myproject.R;
 
 import com.example.myproject.R;
 
@@ -16,7 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button quit = findViewById(R.id.quit);
+        Button startButton = findViewById(R.id.start);
 
         quit.setOnClickListener(v -> finish());
+
+        startButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
