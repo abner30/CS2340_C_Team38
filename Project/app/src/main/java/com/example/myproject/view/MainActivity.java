43e4474611d6cd9c44d11026a015095df1be2850
ayaanajaney <ayaan.ajaney@gmail.com>
2024-10-04 17:55:57 -0400

    Update MainActivity.java

--- Project/app/src/main/java/com/example/myproject/view/MainActivity.java
+++ Project/app/src/main/java/com/example/myproject/view/MainActivity.java
@@ -8,8 +8,6 @@ import com.example.myproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
