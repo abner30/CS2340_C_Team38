f4d7cf3d3d293f4abfc48a79fe84e550b19d9825
kb8767 <krishnahbhanderi@gmail.com>
2024-10-04 02:43:42 -0400

    Adding functionality to quit button, closes app

--- Project/app/src/main/java/com/example/myproject/view/MainActivity.java
+++ Project/app/src/main/java/com/example/myproject/view/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myproject.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.myproject.R;
 
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button quit = findViewById(R.id.quit);
+
+        quit.setOnClickListener(v -> finish());
     }
 }
