ba3232d9be9ccadd70212c2a5883d648f0e02ff5
Li-Chen-Chang <lchang78@gatech.edu>
2024-10-05 14:25:38 -0400

    Edit to make checkstyle for TravelCommunity.java

--- Project/app/src/main/java/com/example/myproject/view/TravelCommunity.java
+++ Project/app/src/main/java/com/example/myproject/view/TravelCommunity.java
@@ -12,14 +12,21 @@ import com.example.myproject.R;
 
 public class TravelCommunity extends AppCompatActivity {
 
+    /**
+     * This method runs on create.
+     * @param savedInstanceState
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_travel_community);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(
+                    WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top,
+                    systemBars.right, systemBars.bottom);
             return insets;
         });
     }
