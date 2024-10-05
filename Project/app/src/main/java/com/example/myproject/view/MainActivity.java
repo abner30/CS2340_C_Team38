955a0d478b95fa7fd46a1079a728b02110c3e650
kb8767 <krishnahbhanderi@gmail.com>
2024-10-05 14:55:18 -0400

    Conflict Prevention
    Using checkstyle fixed code to prevent major merge conflicts later

--- Project/app/src/main/java/com/example/myproject/view/MainActivity.java
+++ Project/app/src/main/java/com/example/myproject/view/MainActivity.java
@@ -1,40 +1,36 @@
+
 package com.example.myproject.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.myproject.R;
 
 public class MainActivity extends AppCompatActivity {
-    Button quitButton;
-    Button startButton;
-
+    /**
+     * The method runs on create. It controls the whole activity.
+     *
+     * @param savedInstanceState
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        quitButton = findViewById(R.id.quit);
-        startButton = findViewById(R.id.start);
+        Button quit = findViewById(R.id.quit);
+        Button startButton = findViewById(R.id.start);
 
-        quitButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Log.d("MainActivity", "Quit button clicked");
-                finish();
-            }
-        });
+        quit.setOnClickListener(v -> finish());
 
-        startButton.setOnClickListener(new View.OnClickListener(){
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Log.d("MainActivity", "Start button clicked");
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            public void onClick(final View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        LoginActivity.class);
                 startActivity(intent);
             }
         });
