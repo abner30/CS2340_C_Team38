df42713544f8bd9b8b06ac6acf98498ced87e5b5
Li-Chen-Chang <lchang78@gatech.edu>
2024-10-08 19:51:59 -0400

    edit all java files to adapt to checkstyle.

--- Project/app/src/main/java/com/example/myproject/view/LoginActivity.java
+++ Project/app/src/main/java/com/example/myproject/view/LoginActivity.java
@@ -45,17 +45,17 @@ public class LoginActivity extends AppCompatActivity {
     /**
      * If user already exists/logged in, then skip login process.
      */
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        if (currentUser != null) {
-//            Intent intent = new Intent(getApplicationContext(),
-//                    NavBar.class);
-//            startActivity(intent);
-//            finish();
-//        }
-//    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(),
+                    NavBar.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     /**
      * The method runs on create. It controls the whole activity.
