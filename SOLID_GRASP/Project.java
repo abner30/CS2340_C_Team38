3a366c28e2992b3e082d6176d62e2108828ff22f
Li-Chen-Chang <lchang78@gatech.edu>
2024-10-10 21:18:59 -0400

    Addes more methods to Project.java

--- SOLID_GRASP/Project.java
+++ SOLID_GRASP/Project.java
@@ -1,4 +1,3 @@
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +9,8 @@ class Project {
     private List<Task> tasks;
     private List<TeamMember> teamMembers;
 
-    public Project(String name, String description, String startDate, String endDate) {
+    public Project(String name, String description,
+                   String startDate, String endDate) {
         this.name = name;
         this.description = description;
         this.startDate = startDate;
@@ -32,13 +32,35 @@ class Project {
         teamMembers.remove(member);
     }
 
+    public String getName() {
+        return name;
+    }
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
+    public String getDescription() {
+        return description;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    //Potentially add getter for tasks and team members
+    //Add to Strings to other classes to make it return something meaningful.
     public boolean isComplete() {
         boolean allComplete = true;
         for (int i = 0; i < tasks.size(); i++) {
