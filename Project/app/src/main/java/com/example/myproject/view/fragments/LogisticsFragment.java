8e32f8695bf6bc3d95357cd75e52dfb22818c0a1
kb8767 <krishnahbhanderi@gmail.com>
2024-10-25 18:10:03 -0400

    Update Gradle and Working Import!
    github chart import finally working yay!

--- Project/app/src/main/java/com/example/myproject/view/fragments/LogisticsFragment.java
+++ Project/app/src/main/java/com/example/myproject/view/fragments/LogisticsFragment.java
@@ -1,24 +1,23 @@
 package com.example.myproject.view.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.LinearLayout;
+
+
 import androidx.fragment.app.Fragment;
 
 import com.example.myproject.R;
+import com.github.mikephil.charting.charts.PieChart;
 
-//import java.util.ArrayList;
-//import com.github.mikephil.charting.charts.PieChart;
-//import com.github.mikephil.charting.data.PieData;
-//import com.github.mikephil.charting.data.PieDataSet;
-//import com.github.mikephil.charting.data.PieEntry;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class LogisticsFragment extends Fragment {
-    //private PieChart pieChart;
-
     /**
      * This method runs on create.
      * @param inflater The LayoutInflater object that can be used to inflate
@@ -38,26 +37,5 @@ public class LogisticsFragment extends Fragment {
                              final Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_logistics,
                 container, false);
-
-        // Initialize the PieChart
-//        pieChart = view.findViewById(R.id.pieChart);
-//        setupPieChart();
-//
-//        return view;
     }
-//    private void setupPieChart() {
-//        // Example data for the pie chart
-//        List<PieEntry> entries = new ArrayList<>();
-//        entries.add(new PieEntry(4, "Allotted Days"));
-//        entries.add(new PieEntry(3, "Remaining Days"));
-//
-//        PieDataSet dataSet = new PieDataSet(entries, "Trip Days");
-//        dataSet.setColors(new int[]{android.graphics.Color.GREEN, android.graphics.Color.RED});
-//
-//        PieData data = new PieData(dataSet);
-//        pieChart.setData(data);
-//        pieChart.invalidate(); // Refresh chart with data
-//    }
 }
-
-
