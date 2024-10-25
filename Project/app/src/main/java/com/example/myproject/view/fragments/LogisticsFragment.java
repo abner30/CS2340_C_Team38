fae32ba1a07443396f274ebac980ee20950be882
kb8767 <krishnahbhanderi@gmail.com>
2024-10-25 17:32:44 -0400

    Current Working App
    Commit with working app and updated logistics page with text and buttons

--- Project/app/src/main/java/com/example/myproject/view/fragments/LogisticsFragment.java
+++ Project/app/src/main/java/com/example/myproject/view/fragments/LogisticsFragment.java
@@ -9,15 +9,15 @@ import androidx.fragment.app.Fragment;
 
 import com.example.myproject.R;
 
-import java.util.ArrayList;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
+//import java.util.ArrayList;
+//import com.github.mikephil.charting.charts.PieChart;
+//import com.github.mikephil.charting.data.PieData;
+//import com.github.mikephil.charting.data.PieDataSet;
+//import com.github.mikephil.charting.data.PieEntry;
 
 
 public class LogisticsFragment extends Fragment {
-    private PieChart pieChart;
+    //private PieChart pieChart;
 
     /**
      * This method runs on create.
@@ -40,24 +40,24 @@ public class LogisticsFragment extends Fragment {
                 container, false);
 
         // Initialize the PieChart
-        pieChart = view.findViewById(R.id.pieChart);
-        setupPieChart();
-
-        return view;
+//        pieChart = view.findViewById(R.id.pieChart);
+//        setupPieChart();
+//
+//        return view;
     }
-    private void setupPieChart() {
-        // Example data for the pie chart
-        List<PieEntry> entries = new ArrayList<>();
-        entries.add(new PieEntry(4, "Allotted Days"));
-        entries.add(new PieEntry(3, "Remaining Days"));
-
-        PieDataSet dataSet = new PieDataSet(entries, "Trip Days");
-        dataSet.setColors(new int[]{android.graphics.Color.GREEN, android.graphics.Color.RED});
-
-        PieData data = new PieData(dataSet);
-        pieChart.setData(data);
-        pieChart.invalidate(); // Refresh chart with data
-    }
+//    private void setupPieChart() {
+//        // Example data for the pie chart
+//        List<PieEntry> entries = new ArrayList<>();
+//        entries.add(new PieEntry(4, "Allotted Days"));
+//        entries.add(new PieEntry(3, "Remaining Days"));
+//
+//        PieDataSet dataSet = new PieDataSet(entries, "Trip Days");
+//        dataSet.setColors(new int[]{android.graphics.Color.GREEN, android.graphics.Color.RED});
+//
+//        PieData data = new PieData(dataSet);
+//        pieChart.setData(data);
+//        pieChart.invalidate(); // Refresh chart with data
+//    }
 }
 
 
